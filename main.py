4ac5a902829ad577e1698db801ee8e3ffa549f77
ethanedits <58463523+ethanedits@users.noreply.github.com>
2020-09-09 01:14:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import win32gui
 import win32con
 from win32api import GetSystemMetrics
 
-pygame.init()
-width = GetSystemMetrics(0)
-height = GetSystemMetrics(1)
-screen = pygame.display.set_mode((width, height), pygame.NOFRAME) #, pygame.FULLSCREEN
-done = False
+pygame.init() #Initializing our pygame window
+width = GetSystemMetrics(0) #Getting computers resolution width
+height = GetSystemMetrics(1) #Getting computers resolution height
+screen = pygame.display.set_mode((width, height), pygame.NOFRAME) #Creating our screen, setting it to NOFRAME so we dont see the windows default UI (Minimize, Fullscreen, Exit)
+done = False #For our while loop
 
 fuchsia = (255, 0, 128)  # Transparency color
 
@@ -17,18 +17,18 @@ hwnd = pygame.display.get_wm_info()["window"]
 
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
                        win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE) #| win32con.SWP_NOMOVE
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY) #Setting window color to transparent
+win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOSIZE) #Setting window to always be on top
 
 while not done:
 
   for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-  screen.fill(fuchsia)
+  screen.fill(fuchsia) #Setting the screen blank every frame so that the drawings will update in realtime
 
-  #drawing functions
-  #Example: 
-  pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(0, 0, 60, 60))
-
-  pygame.display.update()
+  #drawing loop
+  pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(0, 0, 60, 60)) #Example
+  #end drawing loop
+    
+  pygame.display.update() #Updating display every frame so that the drawings will update in realtime
